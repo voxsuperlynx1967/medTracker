3161f2e93def995895f2a588fc67719ada40204c
Simon Carroll <snc40@georgetown.edu>
2020-11-27 17:17:52 -0500

    added in wrong file

---
+++ server/api/users.py
@@ -0,0 +1,59 @@
+from flask import Blueprint, jsonify, request, session
+from flask_jwt_extended import create_access_token
+from app.models import User, db
+import bcrypt
+
+user_routes = Blueprint('users', __name__)
+
+
+def set_password(password):
+    hashed_passwoprd = bcrypt.hashpw(
+        password.encode('utf-8'), bcrypt.gensalt()
+    )
+    return hashed_password
+def verify_password(password, hashed_password):
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    else:
+        return False
+
+@user_routes.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    email = data['email']
+    hashed_password = set_password(data['password'])
+    user = User(
+        email=email,
+        hashed_password=hashed_password
+    )
+    db.session.add(user)
+    db.session.commit()
+    session['musician'] = user.to_dict()
+    user = user.to_dict()
+    return jsonify(auth_token(auth_token=auth_token, user=user))
+
+@user_routes.route('/login', methods=['PUT'])
+def login()
+    data = request.get_json()
+    email = data['email']
+    password = data['password']
+    user = User.query.filter_by(email=email).first()
+    verified = verify_password(password, musician.hashed_password)
+    if not verified:
+        return jsonify(message='Password verify failed')
+    else:
+        auth_token = create_access_token(
+            identitiy={"email": user.email}
+        )
+    session['user'] = user.to_dict()
+    user = user.to_dict()
+    return jsonify(auth_token=auth_token, user=user)
+
+
+@user_routes.route("/session")
+def load_musician():
+  if 'user' in session:
+    musician = session['user']
+    return {"user": session['user']}, 200
+  else:
+    return {"msg": "user not loaded"}, 400
